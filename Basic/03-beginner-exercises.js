d90d6ac75b0baba8ffe221f1313c77aa8cd14a67
Martialk <mfarrus16@gmail.com>
2026-09-12 17:40:15 +0200

    Resolució de ejercicios 3 i 5

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -4,21 +4,87 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 */
 
 // 1. Escribe un comentario en una línea
-
+console.log("Hola Alcanó!")
 // 2. Escribe un comentario en varias líneas
-
+console.log("Hola Alcanó")
+console.log("Avui és 12/09/2026")
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
-
+let Name = "Martí"
+let age = 16
+let man = true 
+let Undefined
+let Null = null
+let MySymbol = Symbol("x")
+let MyBigInt = BigInt(9393939393939393939393939393939393939393939393939393939)
 // 4. Imprime por consola el valor de todas las variables
-
+console.log(Name)
+console.log(age)
+console.log(man)
+console.log(Undefined)
+console.log(Null)
+console.log(MySymbol)
+console.log(MyBigInt)
 // 5. Imprime por consola el tipo de todas las variables
-
+console.log(typeof Name)
+console.log(typeof age)
+console.log(typeof man)
+console.log(typeof Undefined)
+console.log(typeof Null)
+console.log(typeof MySymbol)
+console.log(typeof MyBigInt)
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
-
+Name = "Alcanó"
+age = 2026
+man = false
+MySymbol = Symbol("y")
+MyBigInt = BigInt(77474775656475654747656446375)
+console.log(Name)
+console.log(age)
+console.log(man)
+console.log(MyBigInt)
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
-
+Name = 13
+age = "old"
+man = null
+MySymbol = 22455473457556856723563462354568n
+MyBigInt = Symbol("Martí")
+Undefined = "Martí"
+Null = undefined
+console.log(Name)
+console.log(age)
+console.log(man)
+console.log(Undefined)
+console.log(Null)
+console.log(MySymbol)
+console.log(MyBigInt)
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
-
+const Name2 = "Martí"
+const age2 = 16
+const man2 = true 
+const undefined2 = undefined
+const Null2 = null
+const MySymbol2 = Symbol("x")
+const MyBigInt2 = BigInt(9393939393939393939393939393939393939393939393939393939)
+console.log(Name2)
+console.log(age2)
+console.log(man2)
+console.log(undefined2)
+console.log(Null2)
+console.log(MySymbol2)
+console.log(MyBigInt2)
 // 9. A continuación, modifica los valores de las constantes
-
+//Name2 = "Alcanó"
+//age2 = 20
+//man2 = false 
+//undefined2 = 30
+//Null2 = 15
+//MySymbol2 = Symbol("y")
+//MyBigInt2 = BigInt(9393939929934393939393939393939393939393939393939393939393939)
+console.log(Name2)
+console.log(age2)
+console.log(man2)
+console.log(undefined2)
+console.log(Null2)
+console.log(MySymbol2)
+console.log(MyBigInt2)
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -4,22 +4,56 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
 */
 
 // 1. Crea una variable para cada operación aritmética
-
+let a = 10
+let b = 5
+let suma = a+b
+let resta = a-b
+let multiplicació = a*b
+let potència = a**b
+let divisió = a/b
+let residu = a%b
+console.log(suma)
+console.log(resta)
+console.log(multiplicació)
+console.log(potència)
+console.log(divisió)
+console.log(residu)
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
-
+let suma2 = suma += b
+let resta2 = resta -= b
+let multiplicació2 = multiplicació *= b
+let potència2 = potència **= b
+let divisió2 = divisió /= b
+let residu2 = residu %= b
+console.log(suma2)
+console.log(resta2)
+console.log(multiplicació2)
+console.log(potència2)
+console.log(divisió2)
+console.log(residu2)
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
-
+console.log(5 > 4)
+console.log(4 < 7)
+console.log(2 >= 2)
+console.log(2 <= 4)
+console.log(2 == "2")
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
-
+console.log(2 === "2")
+console.log(2 !== 2)
+console.log(2 == 4)
+console.log(2 != "2")
+console.log(undefined === null)
 // 5. Utiliza el operador lógico and
-
+console.log(15>10 && 3<7)
 // 6. Utiliza el operador lógico or
-
+console.log(11>0 || 4<3)
 // 7. Combina ambos operadores lógicos
-
+console.log(15<10 && 3>1 || 1 == 1)
 // 8. Añade alguna negación
-
+console.log(1 != 3)
 // 9. Utiliza el operador ternario
-
+let man = true
+man ? console.log("És un home.") : console.log("No és un home.")
 // 10. Combina operadores aritméticos, de comparáción y lógicas
+console.log(3+5 > 2 && 5-15 > 3 || !(3 >= 4))
